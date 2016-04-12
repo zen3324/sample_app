394f79797935b767760cfdcf50d0fe9bd68f90d1
Zen <innovationlife.net@gmail.com>
2016-04-12 15:07:48 +0900

    Replace will_paginate to kaminari

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,10 +17,10 @@ describe 'UserPages' do
       before(:all) { 30.times { FactoryGirl.create(:user) } }
       after(:all) { User.delete_all }
 
-      it { should have_selector('div.pagination') }
+      it { should have_selector('nav.pagination') }
 
       it 'should list each user' do
-        User.paginate(page: 1).each do |user|
+        User.page(1).each do |user|
           expect(page).to have_selector('li', text: user.name)
         end
       end
